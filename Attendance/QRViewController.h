3fa533bd80133d5e23fe3b4b8c0012269049510f
aabato <angelica.bato@gmail.com>
2016-02-17 16:17:21 -0500

    Add action to go back button for modal dismissal

---
+++ Attendance/QRViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRViewController.h
+//  Attendance
+//
+//  Created by Angelica Bato on 2/17/16.
+//  Copyright © 2016 Angelica Bato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRViewController : UIViewController
+
+@end
